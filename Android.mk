021a3ca3a07d63bdd619951cdff52af65670571e
GitItsyourt7 <79563281+GitItsyourt7@users.noreply.github.com>
2021-02-23 23:08:07 -0800

    Webmaster Update For Android From GIT!
    Akame Gabby CC xx HH xx xx CC CC r see been g FF CC bunker HH GG off BBBI my dad

--- Android.mk
+++ Android.mk
@@ -1,8 +1,10 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+include $(CLEAR_VARS)(6.7)
 
 LOCAL_MODULE := su
-LOCAL_LDFLAGS := -static
+LOCAL_LDFLAGS := -static_detect_automaticlly
 LOCAL_SRC_FILES := su.c daemon.c utils.c pts.c common.c
 
-include $(BUILD_EXECUTABLE)
+include $(BUILD_EXECUTABLE)(NO_ERRORS )
+Include $(ALL_PLATFORMS)(ANDROID_WEBMASTER_9.9)
+
